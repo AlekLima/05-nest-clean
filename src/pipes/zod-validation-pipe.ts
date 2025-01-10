77518c332baca08ac865f19276f7be34c9bbb458
AlekLima <167828616+AlekLima@users.noreply.github.com>
2025-01-10 16:50:31 -0300

    Commit: Criando Pipe de validação do Zod
    Command Line:
    "pnpm i  zod",
    'pnpm i zod-validation-error'

---
+++ src/pipes/zod-validation-pipe.ts
@@ -0,0 +1,26 @@
+import { PipeTransform,BadRequestException,} from '@nestjs/common'
+import { fromZodError } from 'zod-validation-error'
+import { ZodError, ZodSchema } from 'zod'
+
+ export class  ZodValidationPipe implements PipeTransform {
+    constructor(private  schema: ZodSchema)  {}
+
+    transform(value: unknown ) {
+        try {
+            this.schema.parse(value)
+        } catch (error) {
+            if (error instanceof ZodError) {
+                throw new BadRequestException({
+                    message:'Validation Failed',
+                    statusCode: 400,
+                    error: fromZodError(error),
+                })
+
+            }
+
+            throw new BadRequestException('Validation failed')
+        }
+
+        return value
+    }
+ }
